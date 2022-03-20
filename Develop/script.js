633e397b217eb722ec38a263ec29a63cf3d91af0
bennettem <emily.pamelabennett@gmail.com>
2022-03-20 18:18:54 -0500

    global vars declared

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,92 @@
 // Assignment code here
-
+// declare global vars
+var lowercase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var uppercase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = [
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "-",
+  "_",
+  "+",
+  "=",
+  "[",
+  "]",
+  "{",
+  "}",
+  "|",
+  "/",
+  "?",
+  "<",
+  ">",
+  ";",
+  ":",
+];
+//create prompts/confirms
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,7 +97,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
