d3a43d54b92297ae65132fac18031ae67974a13a
bennettem <emily.pamelabennett@gmail.com>
2022-03-20 19:21:25 -0500

    fixing storage and button issues

--- Develop/script.js
+++ Develop/script.js
@@ -86,53 +86,49 @@ var specialChar = [
   ";",
   ":",
 ];
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
 //create prompts/confirms
-function createPassword() {
-  var passwordArray = [];
+function generatePassword() {
   var password = "";
+  var passwordArray = [];
   var characterLength = prompt(
     "How long would you like your password? *Must be between 8 to 128 characters long"
   );
   // add stop if criteria doesn't match 8-128
   if (characterLength < 8 || characterLength > 128) {
     alert("Please provide a valid option!");
-    createPassword();
-  } else {
-    var confirmLowercase = confirm(
-      "Would you like to include lowercase characters?"
-    );
-    var confirmUppercase = confirm(
-      "Would you like to include uppercase characters?"
-    );
-    var confirmNumeric = confirm("Would you like to include numbers?");
-    var confirmSpecial = confirm(
-      "Would you like to include special characters?"
-    );
-    // add condition if 'cancel' is clicked
-    if (
-      confirmLowercase === false &&
-      confirmUppercase === false &&
-      confirmNumeric === false &&
-      confirmSpecial === false
-    ) {
-      alert(
-        "Invalid entry please try again. Must choose at least one criteria"
-      );
-      createPassword();
-    }
+    return generatePassword();
   }
-  // create new array based on user input
+  var confirmLowercase = confirm(
+    "Would you like to include lowercase characters?"
+  );
   if (confirmLowercase) {
     passwordArray = passwordArray.concat(lowercase);
   }
+  var confirmUppercase = confirm(
+    "Would you like to include uppercase characters?"
+  );
   if (confirmUppercase) {
     passwordArray = passwordArray.concat(uppercase);
   }
+  var confirmNumeric = confirm("Would you like to include numbers?");
   if (confirmNumeric) {
     passwordArray = passwordArray.concat(numeric);
   }
+  var confirmSpecial = confirm("Would you like to include special characters?");
   if (confirmSpecial) {
     passwordArray = passwordArray.concat(specialChar);
+  }
+  // add condition if 'cancel' is clicked
+  if (
+    confirmLowercase === false &&
+    confirmUppercase === false &&
+    confirmNumeric === false &&
+    confirmSpecial === false
+  ) {
+    alert("Invalid entry please try again. Must choose at least one criteria");
+    return generatePassword();
   }
   // for loop to generate randomized password
   for (var i = 0; i < characterLength; i++) {
@@ -141,14 +137,10 @@ function createPassword() {
       var newIndex = passwordArray[randomIndex];
       return newIndex;
     }
-    password += newPassword();
+    password = password + newPassword();
   }
   return password;
 }
-createPassword();
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
